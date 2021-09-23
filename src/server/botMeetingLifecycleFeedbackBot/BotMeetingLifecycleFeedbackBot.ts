e26a61857ef07aef9cd4252a46a8a27a9781d615
mmsharepoint <markus.moeller.ed@outlook.de>
2021-09-23 13:31:54 +0200

    UAM implemented, bot functions updated

--- src/server/botMeetingLifecycleFeedbackBot/BotMeetingLifecycleFeedbackBot.ts
+++ src/server/botMeetingLifecycleFeedbackBot/BotMeetingLifecycleFeedbackBot.ts
@@ -1,6 +1,8 @@
 import { BotDeclaration } from "express-msteams-host";
 import * as debug from "debug";
-import { StatePropertyAccessor, CardFactory, TurnContext, MemoryStorage, ConversationState, ActivityTypes, TeamsActivityHandler } from "botbuilder";
+import { StatePropertyAccessor, CardFactory, TurnContext, MemoryStorage, ConversationState, ActivityTypes, TeamsActivityHandler, MessageFactory, ResourceResponse, StatusCodes, InvokeResponse } from "botbuilder";
+import AdaptiveCardSvc from "../../services/AdaptiveCardSvc";
+import { Feedback } from "../../models/Feedback";
 // Initialize debug logging module
 const log = debug("msteams");
 
@@ -24,15 +26,129 @@ export class BotMeetingLifecycleFeedbackBot extends TeamsActivityHandler {
         super();
     }
 
-    async onTurnActivity(context) {
+    async onInvokeActivity(context: TurnContext): Promise<InvokeResponse<any>> {
+        if (context.activity.value.action.verb === "alreadyVoted") {
+            const persistedFeedback: Feedback = context.activity.value.action.data.feedback;
+            const card = AdaptiveCardSvc.getDisabledCard(persistedFeedback);
+            const cardRes = {
+                statusCode: StatusCodes.OK,
+                type: 'application/vnd.microsoft.card.adaptive',
+                value: card
+            };
+            const res = {
+                status: StatusCodes.OK,
+                body: cardRes
+            };
+            return res;
+        }
+
+        if (context.activity.type == 'invoke') {
+            log(context.activity.value.action.data);
+            log(context.activity.value.action.verb);
+            log(context.activity.from.aadObjectId);
+            log(context.activity.from.name);
+            const persistedFeedback: Feedback = context.activity.value.action.data.feedback;
+            persistedFeedback.votedPersons.push(context.activity.from.aadObjectId!);
+            switch (context.activity.value.action.verb) {
+                case "vote_1":
+                    persistedFeedback.votes1 += 1;
+                    break;
+                case "vote_2":
+                    persistedFeedback.votes2 += 1;
+                    break;
+                case "vote_3":
+                    persistedFeedback.votes3 += 1;
+                    break;
+                case "vote_4":
+                    persistedFeedback.votes4 += 1;
+                    break;
+                case "vote_5":
+                    persistedFeedback.votes5 += 1;
+                    break;
+            };
+            const card = CardFactory.adaptiveCard(AdaptiveCardSvc.getCurrentCard(persistedFeedback));
+            const message = MessageFactory.attachment(card);
+            message.id = context.activity.replyToId;
+            
+            var response = await context.updateActivity(message);
+            // return Promise.resolve(response);
+            // const cardRes = {
+            //     statusCode: StatusCodes.OK,
+            //     type: 'application/vnd.microsoft.card.adaptive',
+            //     value: card
+            // };
+            // const res = {
+            //     status: StatusCodes.OK,
+            //     body: cardRes
+            // };
+        }
+
+        return {
+            status: StatusCodes.OK
+        };
+    };
+
+    async onEventActivity(context) {
         if (context.activity.type == 'event' && context.activity.name == "application/vnd.microsoft.meetingStart") {
             var meetingObject = context.activity.value;
-            await context.sendActivity(`Meeting ${meetingObject.Title} started at ${meetingObject.StartTime}`);
+            const card = CardFactory.adaptiveCard(AdaptiveCardSvc.getInitialCard(meetingObject.Id));
+            const message = MessageFactory.attachment(card);
+            await context.sendActivity(message);
         }
     
         if (context.activity.type == 'event' && context.activity.name == "application/vnd.microsoft.meetingEnd") {
             var meetingObject = context.activity.value;
             await context.sendActivity(`Meeting ${meetingObject.Title} ended at ${meetingObject.EndTime}`);
         }
-    };
+    };   
+    // async onTurnActivity(context) {
+        
+
+    //     if (context.activity.value.action.verb === "alreadyVoted") {
+    //         const persistedFeedback: Feedback = context.activity.value.action.data.feedback;
+    //         const card = AdaptiveCardSvc.getDisabledCard(persistedFeedback);
+    //         const cardRes = {
+    //             statusCode: StatusCodes.OK,
+    //             type: 'application/vnd.microsoft.card.adaptive',
+    //             value: card
+    //         };
+    //         const res = {
+    //             status: StatusCodes.OK,
+    //             body: cardRes
+    //         };
+    //         return Promise.resolve(res);
+    //     }
+
+    //     if (context.activity.type == 'invoke') {
+    //         log(context.activity.value.action.data);
+    //         log(context.activity.value.action.verb);
+    //         log(context.activity.from.aadObjectId);
+    //         log(context.activity.from.name);
+    //         const persistedFeedback: Feedback = context.activity.value.action.data.feedback;
+    //         persistedFeedback.votedPersons.push(context.activity.from.aadObjectId);
+    //         switch (context.activity.value.action.verb) {
+    //             case "vote_1":
+    //                 persistedFeedback.votes1 += 1;
+    //                 break;
+    //             case "vote_2":
+    //                 persistedFeedback.votes2 += 1;
+    //                 break;
+    //             case "vote_3":
+    //                 persistedFeedback.votes3 += 1;
+    //                 break;
+    //             case "vote_4":
+    //                 persistedFeedback.votes4 += 1;
+    //                 break;
+    //             case "vote_5":
+    //                 persistedFeedback.votes5 += 1;
+    //                 break;
+    //         };
+    //         const card = CardFactory.adaptiveCard(AdaptiveCardSvc.getCurrentCard(persistedFeedback));
+    //         const message = MessageFactory.attachment(card);
+    //         message.id = context.activity.replyToId;
+            
+    //         var response = await context.updateActivity(message);
+    //         return Promise.resolve(response);
+    //     }
+    // };
 }
---
+++ src/services/AdaptiveCardSvc.ts
@@ -0,0 +1,123 @@
+import { Feedback } from "../models/Feedback";
+
+export default class AdaptiveCardSvc { 
+    private static initialFeedback: Feedback = {
+        meetingID: "",
+        votedPersons: [],
+        votes1: 0,
+        votes2: 0,
+        votes3: 0,
+        votes4: 0,
+        votes5: 0
+    };
+
+    private static basicCard = {
+        type: "AdaptiveCard",
+        schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+        version: "1.4",
+        refresh: {
+            action: {
+                type: "Action.Execute",
+                title: "Refresh",
+                verb: "alreadyVoted",
+                data: {
+                      feedback: {}
+                }
+            },
+            userIds: ["00000000-0000-0000-0000-000000000000"]
+        },
+        body: [
+            {
+                type: "TextBlock",
+                text: "How did you like the meeting?",
+                wrap: true
+            },
+            {
+                type: "ActionSet",
+                actions: []
+            }
+        ]
+    };
+
+    private static votingActions = [
+        {
+            type: "Action.Execute",
+            title: " ",
+            verb: "vote_1",
+            iconUrl: "https://mmodocumentapproval.azurewebsites.net/assets/icon.png",
+            data: {
+                feedback: {}
+            }
+        },
+        {
+            type: "Action.Execute",
+            title: " ",
+            verb: "vote_2",
+            iconUrl: "https://mmodocumentapproval.azurewebsites.net/assets/icon.png",
+            data: {
+                feedback: {}
+            }
+        },
+        {
+            type: "Action.Execute",
+            title: " ",
+            verb: "vote_3",
+            iconUrl: "https://mmodocumentapproval.azurewebsites.net/assets/icon.png",
+            data: {
+                feedback: {}
+            }
+        },
+        {
+            type: "Action.Execute",
+            title: " ",
+            verb: "vote_4",
+            iconUrl: "https://mmodocumentapproval.azurewebsites.net/assets/icon.png",
+            data: {
+                feedback: {}
+            }
+        },
+        {
+            type: "Action.Execute",
+            title: " ",
+            verb: "vote_5",
+            iconUrl: "https://mmodocumentapproval.azurewebsites.net/assets/icon.png",
+            data: {
+                feedback: {}
+            }
+        }
+    ];
+
+    public static getInitialCard(meetingID: string) {
+        let initialFeedback = this.initialFeedback;
+        initialFeedback.meetingID = meetingID;
+        const card: any = this.basicCard;
+        card.body[1].actions = this.votingActions;
+        card.body[1].actions[0].data.feedback = initialFeedback;
+        card.body[1].actions[1].data.feedback = initialFeedback;
+        card.body[1].actions[2].data.feedback = initialFeedback;
+        card.body[1].actions[3].data.feedback = initialFeedback;
+        card.body[1].actions[4].data.feedback = initialFeedback;
+        card.refresh.action.data.feedback = initialFeedback;
+        return card;
+    }
+
+    public static getCurrentCard(feedback: Feedback) {
+        const card: any = this.basicCard;
+        card.body[1].actions = this.votingActions;
+        card.body[1].actions[0].data.feedback = feedback;
+        card.body[1].actions[1].data.feedback = feedback;
+        card.body[1].actions[2].data.feedback = feedback;
+        card.body[1].actions[3].data.feedback = feedback;
+        card.body[1].actions[4].data.feedback = feedback;
+        card.refresh.action.data.feedback = feedback;
+        card.refresh.userIds = feedback.votedPersons;
+        return card;
+    }
+
+    public static getDisabledCard(feedback: Feedback) {
+        const card: any = this.basicCard;
+        card.body[1].actions = [];
+        card.refresh.action.data.feedback = feedback;
+        return card;
+    }
+}
